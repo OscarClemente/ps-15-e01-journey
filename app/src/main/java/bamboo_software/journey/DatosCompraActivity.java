33103ebf7155c184b894e78c94e8b5ebba2e9e9e
Jorge Cáncer GIl <646122@unizar.es>
2015-05-15 16:06:21 +0200

    Diversas Mejoras

--- app/src/main/java/bamboo_software/journey/DatosCompraActivity.java
+++ app/src/main/java/bamboo_software/journey/DatosCompraActivity.java
@@ -1,10 +1,8 @@
 package bamboo_software.journey;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -73,7 +71,7 @@ public class DatosCompraActivity extends Activity {
                 mail.enviar(getUsuario());
 
                 setResult(RESULT_OK);
-                //finish();
+                finish();
             }
         });
     }
